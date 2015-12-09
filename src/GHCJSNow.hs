4112cb7296dae60569032a964197688d8470a907
Ivan Perez <ivan.perez@keera.co.uk>
2015-12-09 23:17:53 +0000

    Uses getPOSIXTime (closes #4)

--- src/GHCJSNow.hs
+++ src/GHCJSNow.hs
@@ -1,7 +1,8 @@
 module GHCJSNow where
 
 import Data.IORef
-import JsImports                     (now)
+import Data.Time.Clock.POSIX (getPOSIXTime)
+-- import JsImports                     (now)
 
 initializeTimeRef :: IO (IORef Int)
 initializeTimeRef = do
@@ -14,7 +15,7 @@ initializeTimeRef = do
 senseTimeRef :: IORef Int -> IO Int
 senseTimeRef timeRef = do
   -- Get time passed since SDL init
-  newTime <- fmap round now
+  newTime <- fmap secsToMilisecs getPOSIXTime
 
   -- Obtain time difference
   dt <- updateTime timeRef newTime
@@ -26,6 +27,9 @@ updateTime timeRef newTime = do
   previousTime <- readIORef timeRef
   writeIORef timeRef newTime
   return (newTime - previousTime)
+
+secsToMilisecs :: RealFrac a => a -> Int
+secsToMilisecs m = round (m * 1000)
 
 milisecsToSecs :: Int -> Double
 milisecsToSecs m = fromIntegral m / 1000
