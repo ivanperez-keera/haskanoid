83915e849a320a12a3bd2bf2b3cbb4eb070ec778
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-02 21:55:15 +0000

    Add space after comma (KSG Haskell 1.3.0 - 4.5.3). Refs #103.
    Beyond where required by the language or other style rules, and apart
    from literals and comments, a single ASCII space should also appear
    after the comma (,) in lists and tuples.

--- src/Data/Extra/Ord.hs
+++ src/Data/Extra/Ord.hs
@@ -6,5 +6,5 @@
 -- Auxiliary functions related to the 'Ord' typeclass.
 module Data.Extra.Ord where
 
-inRange :: Ord a => (a,a) -> a -> a
+inRange :: Ord a => (a, a) -> a -> a
 inRange (mN, mX) x = min mX (max mN x)
