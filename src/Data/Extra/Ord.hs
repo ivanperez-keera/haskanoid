6a95936e0cc5e9017c7acaec4dafeefd70cef3bc
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-02 15:00:15 +0000

    Add module copyright (KSG Haskell 1.3.0 - 3.3). Refs #103.
    All Haskell files must include a copyright notice in a comment. The
    copyright notice must be included in a format parseable by Haddock.

--- src/Data/Extra/Ord.hs
+++ src/Data/Extra/Ord.hs
@@ -1,4 +1,9 @@
--- | Auxiliary functions related to the 'Ord' typeclass.
+-- |
+-- Copyright  : (c) Ivan Perez & Henrik Nilsson, 2014.
+-- License    : See LICENSE file.
+-- Maintainer : Ivan Perez <ivan.perez@keera.co.uk>
+--
+-- Auxiliary functions related to the 'Ord' typeclass.
 module Data.Extra.Ord where
 
 inRange :: Ord a => (a,a) -> a -> a
