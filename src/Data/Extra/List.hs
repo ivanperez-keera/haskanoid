66b3309d129067bf68fc712a2ba2964dc46d2be0
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-02 15:00:08 +0000

    Add module copyright (KSG Haskell 1.3.0 - 3.3). Refs #103.
    All Haskell files must include a copyright notice in a comment. The
    copyright notice must be included in a format parseable by Haddock.

--- src/Data/Extra/List.hs
+++ src/Data/Extra/List.hs
@@ -1,4 +1,9 @@
--- | Auxiliary functions related to Data.List.
+-- |
+-- Copyright  : (c) Ivan Perez & Henrik Nilsson, 2014.
+-- License    : See LICENSE file.
+-- Maintainer : Ivan Perez <ivan.perez@keera.co.uk>
+--
+-- Auxiliary functions related to Data.List.
 module Data.Extra.List where
 
 mapFilter :: (a -> b) -> (a -> Bool) -> [a] -> [b]
