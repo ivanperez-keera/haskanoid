f2c56e87b560c291a223a303eb7e32caa579775c
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-02 15:00:19 +0000

    Add module copyright (KSG Haskell 1.3.0 - 3.3). Refs #103.
    All Haskell files must include a copyright notice in a comment. The
    copyright notice must be included in a format parseable by Haddock.

--- src/Data/Extra/VectorSpace.hs
+++ src/Data/Extra/VectorSpace.hs
@@ -1,4 +1,9 @@
--- | Auxiliary functions related to Data.VectorSpace.
+-- |
+-- Copyright  : (c) Ivan Perez & Henrik Nilsson, 2014.
+-- License    : See LICENSE file.
+-- Maintainer : Ivan Perez <ivan.perez@keera.co.uk>
+--
+-- Auxiliary functions related to Data.VectorSpace.
 module Data.Extra.VectorSpace where
 
 import Data.VectorSpace
