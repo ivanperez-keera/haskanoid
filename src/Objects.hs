dd03fa1071f7e0ab386a9b1f1cc9069312080979
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-02 15:51:01 +0000

    Indent to 2 spaces (KSG Haskell 1.3.0 - 4.2). Refs #103.
    Each time a new expression or block-like construct is opened, the indent
    should increase by two spaces. When the section ends, the indent returns
    to the previous indent level. The indent level applies to both Haskell
    expressions and comments throughout the block.

--- src/Objects.hs
+++ src/Objects.hs
@@ -36,7 +36,7 @@ data Object = Object { objectName           :: ObjectName
                      , collisionEnergy      :: Double
                      , displacedOnCollision :: Bool       -- Theoretically, setting cE == 0 should suffice
                      }
- deriving (Show)
+  deriving (Show)
 
 type Objects   = [Object]
 
@@ -67,18 +67,18 @@ isPaddle o = case objectKind o of
 
 objShape :: Object -> Shape
 objShape obj = case objectKind obj of
-  (Ball r)    -> Rectangle (p ^-^ (r,r)) (2*r, 2*r)
-  (Paddle s)  -> Rectangle p s
-  (Block _ s) -> Rectangle p s
-  (Side   s)  -> sideToShape p s
- where p = objectPos obj
-       width'  = gameWidth
-       height' = gameHeight
-       d = collisionErrorMargin
-       sideToShape p TopSide    = Rectangle (p ^-^ (d, d)) (width' + 2*d, d)
-       sideToShape p LeftSide   = Rectangle (p ^-^ (d, d)) (d, height' + 2*d)
-       sideToShape p RightSide  = Rectangle (p ^-^ (0, d)) (d, height' + 2*d)
-       sideToShape p BottomSide = Rectangle (p ^-^ (d, 0)) (width' + 2*d, d)
+    (Ball r)    -> Rectangle (p ^-^ (r,r)) (2*r, 2*r)
+    (Paddle s)  -> Rectangle p s
+    (Block _ s) -> Rectangle p s
+    (Side   s)  -> sideToShape p s
+  where p = objectPos obj
+        width'  = gameWidth
+        height' = gameHeight
+        d = collisionErrorMargin
+        sideToShape p TopSide    = Rectangle (p ^-^ (d, d)) (width' + 2*d, d)
+        sideToShape p LeftSide   = Rectangle (p ^-^ (d, d)) (d, height' + 2*d)
+        sideToShape p RightSide  = Rectangle (p ^-^ (0, d)) (d, height' + 2*d)
+        sideToShape p BottomSide = Rectangle (p ^-^ (d, 0)) (width' + 2*d, d)
 
 -- * Collisions
 type Collisions = [Collision]
@@ -89,8 +89,8 @@ type Collisions = [Collision]
 -- Take into account that the same object could take part in several
 -- simultaneous collitions, so these velocities should be added (per object).
 data Collision = Collision
-  { collisionData :: [(ObjectName, Vel2D)] } -- ObjectId x Velocity
- deriving Show
+    { collisionData :: [(ObjectName, Vel2D)] } -- ObjectId x Velocity
+  deriving Show
 
 -- | Detects a collision between one object and another regardless of
 -- everything else
@@ -109,8 +109,8 @@ collisionSide obj1 obj2 = shapeCollisionSide (objShape obj1) (objShape obj2)
 
 collisionResponseObj :: Object -> Object -> Collision
 collisionResponseObj o1 o2 =
-  Collision $
-    map objectToCollision [(o1, side, o2), (o2, side', o1)]
+    Collision $
+      map objectToCollision [(o1, side, o2), (o2, side', o1)]
   where side  = collisionSide o1 o2
         side' = oppositeSide side
         objectReacts      o             = collisionEnergy o > 0 || displacedOnCollision o
