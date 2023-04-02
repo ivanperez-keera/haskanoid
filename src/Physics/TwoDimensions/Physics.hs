f135efa99834765e3d6a42cd0c8a5b3ee45d7dbc
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-02 15:02:48 +0000

    Add module copyright (KSG Haskell 1.3.0 - 3.3). Refs #103.
    All Haskell files must include a copyright notice in a comment. The
    copyright notice must be included in a format parseable by Haddock.

--- src/Physics/TwoDimensions/Physics.hs
+++ src/Physics/TwoDimensions/Physics.hs
@@ -1,4 +1,9 @@
--- | A very simple physics subsytem. It currently detects shape
+-- |
+-- Copyright  : (c) Ivan Perez & Henrik Nilsson, 2014.
+-- License    : See LICENSE file.
+-- Maintainer : Ivan Perez <ivan.perez@keera.co.uk>
+--
+-- A very simple physics subsytem. It currently detects shape
 -- overlaps only, the actual physics movement is carried out
 -- in Yampa itself, as it is very simple using integrals and
 -- derivatives.
