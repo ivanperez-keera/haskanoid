3e359f214bb0a1967864bf65a12f8f2f939d3b93
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-02 15:02:45 +0000

    Add module copyright (KSG Haskell 1.3.0 - 3.3). Refs #103.
    All Haskell files must include a copyright notice in a comment. The
    copyright notice must be included in a format parseable by Haddock.

--- src/Physics/TwoDimensions/Dimensions.hs
+++ src/Physics/TwoDimensions/Dimensions.hs
@@ -1,5 +1,10 @@
--- | Physical dimensions used all over the game. They are just type synonyms,
--- but it's best to use meaningful names to make our type signatures more
+-- |
+-- Copyright  : (c) Ivan Perez & Henrik Nilsson, 2014.
+-- License    : See LICENSE file.
+-- Maintainer : Ivan Perez <ivan.perez@keera.co.uk>
+--
+-- Physical dimensions used all over the game. They are just type synonyms, but
+-- it's best to use meaningful names to make our type signatures more
 -- meaningful.
 module Physics.TwoDimensions.Dimensions where
 
